438b915eabaf6f9d5b2813661f759278128e6a21
EC2 Default User <ec2-user@ip-172-31-47-241.ap-northeast-1.compute.internal>
2022-07-05 08:12:21 +0000

    05Jul

--- ruby/fizz_buzz.rb
+++ ruby/fizz_buzz.rb
@@ -1,13 +1,17 @@
 def fizz_buzz(n)
- if n==
-   puts"fizz"
-   falsif n==
-   puts"buzz"
-   false n==
-   puts"#{i}"
+ if n % 15 == 0
+   "Fizzbuzz"
+   elsif n % 5 == 0
+   "Buzz"
+   elsif n % 3 == 0
+   "Fizz"
+   else
+   n.to_s
+ end
 end
 
 puts"数字を入力してください"
-　i = gets.to_i
+input = gets.to_i
 puts "結果は..."
-puts fizz_buzz(i)
+puts fizz_buzz(input)
+
